b50b2a3f0a296242e3a27e80304d47d86f898f69
laursuar <laura.suarez@tufts.edu>
2013-03-04 02:05:19 -0500

    testing for where project

---
+++ wheretesting/where/where.js
@@ -0,0 +1,36 @@
+function start(){
+    var mapOptions = {
+	center: new google.maps.LatLng(42.3583, 71.0603),
+	zoom: 8,
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+
+function mapStations(){
+    console.log('mapStations');
+}
+
+function detectLocation(){
+    console.log('detectLocation');
+}
+
+function displayT(){
+    console.log('displayT');
+}
+
+function subwayClick(){
+    console.log('subwayClick');
+}
+
+function carmen(){
+    console.log('carmen');
+}
+
+function waldo(){
+    console.log('waldo');
+}
+
+function calcDistance(){
+    console.log('calcDistance');
+}
